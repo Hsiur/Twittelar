c44374696792cda32c72cfcb74d8f2d1a618d0c5
Hsiur <ruish10@me.com>
2023-07-25 23:13:53 +0500

    Initial Commit

---
+++ Tatarlar/TatarlarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TatarlarApp.swift
+//  Tatarlar
+//
+//  Created by Руслан Ишмухаметов on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TatarlarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
